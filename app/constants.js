1e5c7a1151f363ab5b9cb2fc5654c7f0ab4f9816
Daksh Rathod <daksh.rathod@dataphion.com>
2020-08-11 12:26:55 +0530

    route changes

--- app/constants.js
+++ app/constants.js
@@ -1,8 +1,13 @@
-const host = process.env.HOST || "https://admin.dataphion.com/";
-const apiExecuteHost = process.env.API_EXEC_HOST || "https://aitester.dataphion.com/api/Runtest";
-const sockethost = process.env.SOCKET_HOST || "https://admin.dataphion.com";
-const image_host = process.env.IMAGE_HOST || "https://aitester.dataphion.com";
-const error_image_host = process.env.ERROR_IMAGE_HOST || "https://admin.dataphion.com";
+const host = "/";
+const sockethost = "/socket.io";
+const image_host = "";
+const error_image_host = "";
+
+// const host = process.env.HOST || "https://admin.dataphion.com/";
+// const apiExecuteHost = process.env.API_EXEC_HOST || "https://aitester.dataphion.com/api/Runtest";
+// const sockethost = process.env.SOCKET_HOST || "https://admin.dataphion.com";
+// const image_host = process.env.IMAGE_HOST || "https://aitester.dataphion.com";
+// const error_image_host = process.env.ERROR_IMAGE_HOST || "https://admin.dataphion.com";
 
 // const host = process.env.HOST || "http://localhost:1337/";
 // const apiExecuteHost = process.env.API_EXEC_HOST || "http://localhost:9501/api/Runtest";
@@ -15,41 +20,41 @@ const constants = {
   socket_url: sockethost,
   image_host: image_host,
   extension_id: "ilifkjninjhohkiembejnfaanokppkaa",
-  apiexecutehost: host + "testcases/api/Runtest",
-  register: host + "auth/local/register",
-  login: host + "auth/local",
-  forgotPassword: host + "auth/forgot-password",
-  resetPassword: host + "auth/reset-password",
-  feedbacks: host + "feedbacks",
-  features: host + "features",
-  environments: host + "environments",
-  upload: host + "upload",
-  dbregistrations: host + "dbregistrations",
-  datasource: host + "datasources",
-  objectrepositories: host + "objectrepositories",
-  testcases: host + "testcases",
-  testcasecomponents: host + "testcasecomponents",
-  testcasegroups: host + "testcasegroups",
-  testsuites: host + "testsuites",
-  testsessionexecutions: host + "testsessionexecutions",
-  swaggerFile: host + "applications/swagger",
-  swaggerUpadte: host + "applications/swaggerupdate",
-  swaggerconfirm: host + "applications/swaggerconfirm",
-  endpointpacks: host + "endpointpacks",
-  endpoints: host + "endpoints",
-  flows: host + "flows",
-  graphql: host + "graphql",
-  dbregistrationsCheck: host + "dbregistrations/check",
-  executequery: host + "dbregistrations/execute",
-  sourceregistration: host + "sourceregistrations",
-  application: host + "applications",
-  healedcomponent: host + "healedcomponents",
-  nativeagents: host + "nativeagents",
-  devicetype: host + "devicetypes/device_validate",
-  relationgraph: host + "relationgraphs",
-  smtpdetails: host + "smtpdetails",
-  jobs: host + "jobs",
-  seleniumConfigure: host + "selenium-configures",
+  apiexecutehost: host + "api/testcases/api/Runtest",
+  register: host + "api/auth/local/register",
+  login: host + "api/auth/local",
+  forgotPassword: host + "api/auth/forgot-password",
+  resetPassword: host + "api/auth/reset-password",
+  feedbacks: host + "api/feedbacks",
+  features: host + "api/features",
+  environments: host + "api/environments",
+  upload: host + "api/upload",
+  dbregistrations: host + "api/dbregistrations",
+  datasource: host + "api/datasources",
+  objectrepositories: host + "api/objectrepositories",
+  testcases: host + "api/testcases",
+  testcasecomponents: host + "api/testcasecomponents",
+  testcasegroups: host + "api/testcasegroups",
+  testsuites: host + "api/testsuites",
+  testsessionexecutions: host + "api/testsessionexecutions",
+  swaggerFile: host + "api/applications/swagger",
+  swaggerUpadte: host + "api/applications/swaggerupdate",
+  swaggerconfirm: host + "api/applications/swaggerconfirm",
+  endpointpacks: host + "api/endpointpacks",
+  endpoints: host + "api/endpoints",
+  flows: host + "api/flows",
+  graphql: host + "api/graphql",
+  dbregistrationsCheck: host + "api/dbregistrations/check",
+  executequery: host + "api/dbregistrations/execute",
+  sourceregistration: host + "api/sourceregistrations",
+  application: host + "api/applications",
+  healedcomponent: host + "api/healedcomponents",
+  nativeagents: host + "api/nativeagents",
+  devicetype: host + "api/devicetypes/device_validate",
+  relationgraph: host + "api/relationgraphs",
+  smtpdetails: host + "api/smtpdetails",
+  jobs: host + "api/jobs",
+  seleniumConfigure: host + "api/selenium-configures",
 
   // colors
   PRIMARY_COLOR: "#1039A6",
